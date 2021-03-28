94ceb81dd64f0ec16a9554e2ec326dd0f058c4e9
fisik-yum <fisik-yum@github.com>
2021-03-28 11:59:12 -0700

    cleaning up

--- main.go
+++ main.go
@@ -72,9 +72,7 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
