df25f6731f7286e08746198b3d4aec339abffcd1
fisik-yum <fisik-yum@github.com>
2021-03-28 22:09:27 -0700

    added muterole checks

--- roles/roles.go
+++ roles/roles.go
@@ -9,12 +9,14 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-func writeTo(guildID string, roleID string) { //extracted to a function so I can make life easier.
+func writeTo(guildID string, muteroleID string) { //extracted to a function so I can make life easier.
 
 	records := [][]string{
-		{roleID},
+		{guildID, muteroleID},
 	}
 
 	f, err := os.Create(filepath.Join(".", "guilds", guildID+".csv"))
@@ -55,4 +57,38 @@ func readDataFromFile(id string) (record []string) { //id is to find the said .c
 
 func readItem(id string, index int) string { // makes it easy to read stored data
 	return readDataFromFile(id)[index]
+}
+
+func checkIfMuteExists(guildID string, roles []*discordgo.Role) bool {
+	if checkIfRecordExists(guildID) {
+		if checkIfIDExists(readItem(guildID, 1), roles) {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+func checkIfRecordExists(id string) bool { //check if the csv file exists
+	if _, err := os.Stat(filepath.Join(".", "users", id+".csv")); err == nil {
+		// path/to/whatever exists
+		return true
+	} else if os.IsNotExist(err) {
+		// path/to/whatever does *not* exist
+		return false
+	} else {
+		// Schrodinger: file may or may not exist. See err for details.
+		return false
+		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+
+	}
+}
+
+func checkIfIDExists(id string, roles []*discordgo.Role) bool { //check if the role ID is still valid
+	for x := 0; x < len(roles); x++ {
+		if id == roles[x].ID {
+			return true
+		}
+	}
+	return false
 }
