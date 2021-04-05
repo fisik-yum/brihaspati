b19a0c7c151755c4d116b17c493cafca30df93d5
fisik-yum <fisik-yum@github.com>
2021-04-04 19:55:23 -0700

    added a ghost ping functionality

--- fun.go
+++ fun.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"brihaspati/utils"
 	"math/rand"
 	"strconv"
 	"time"
@@ -13,5 +14,17 @@ func funStuff(s *discordgo.Session, m *discordgo.MessageCreate) {
 		rand.Seed(time.Now().UnixNano())
 		s.ChannelMessageSend(m.ChannelID, "`"+strconv.Itoa(rand.Intn(6)))
 
+	}
+
+	if utils.Prefix(m.Content, "^ghostping") {
+		if len(m.Mentions) != 0 {
+			s.ChannelMessageDelete(m.ChannelID, m.ID)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "`Mention someone to use this function!`")
+		}
+	}
+
+	if utils.IsMentioned(s.State.User.ID, m.Mentions) {
+		s.ChannelMessageSend(m.ChannelID, "`I HAVE BEEN SUMMONED`")
 	}
 }
