57a40d1525327368b3ab9aede7f8d75a84e1864c
fisik-yum <fisik-yum@github.com>
2021-04-05 12:23:59 -0700

    I have no idea what I changed

--- fun.go
+++ fun.go
@@ -12,15 +12,17 @@ import (
 func funStuff(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == "^rolldice" {
 		rand.Seed(time.Now().UnixNano())
-		s.ChannelMessageSend(m.ChannelID, "`"+strconv.Itoa(rand.Intn(6)))
+		s.ChannelMessageSend(m.ChannelID, "`"+strconv.Itoa(rand.Intn(6))+"`")
 
 	}
 
 	if utils.Prefix(m.Content, "^ghostping") {
 		if len(m.Mentions) != 0 {
 			s.ChannelMessageDelete(m.ChannelID, m.ID)
+			return
 		} else {
 			s.ChannelMessageSend(m.ChannelID, "`Mention someone to use this function!`")
+			return
 		}
 	}
 
